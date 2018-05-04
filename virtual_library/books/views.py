d25f0ee483ff3a80e2a1e487898a4605d66a659c
AlexECX <33321263+AlexECX@users.noreply.github.com>
2018-05-03 23:24:42 -0400

    redistribution des fichiers global static vers les apps

--- virtual_library/books/views.py
+++ virtual_library/books/views.py
@@ -1,20 +1,20 @@
 from django.shortcuts import render, redirect
 
-
 from books.models import Book, Author, Genre
+
 # from books.models import Book_Author
 # Create your views here.
 
 
-def search(request, keyword:str=None):
+def search(request, keyword: str = None):
 
-    if keyword == None:
+    if keyword is None:
         try:
-            keyword=request.POST['research']
+            keyword = request.POST['research']
             return redirect("books:search", keyword)
-            #search(keyword)
-        except (KeyError):
-            context = { 'error_message': "Error in the search" }
+            # search(keyword)
+        except KeyError:
+            context = {'error_message': "Error in the search"}
             return render(request, 'books/search_result.html', context)
     else:
         keyword = keyword.replace("%20", " ")
@@ -22,25 +22,25 @@ def search(request, keyword:str=None):
         books = []
         authors = []
 
-        #The keyword is a name of an author
+        # The keyword is a name of an author
         # books_by_author = Book.objects.filter(authors__name__icontains=keyword)
         # if books_by_author:
         #     for b1 in books_by_author:
         #         books.append((b1.id, b1.title))
 
-        #The keyword is a title of a book
+        # The keyword is a title of a book
         books_by_title = Book.objects.filter(title__icontains=keyword)
         if books_by_title:
             for b2 in books_by_title:
                 books.append((b2.id, b2.title))
 
-        #The keyword is a genre
+        # The keyword is a genre
         # books_by_genre = Book.objects.filter(genres__name__icontains=keyword)
         # if books_by_genre:
         #     for b3 in books_by_genre:
         #         books.append((b3.id, b3.title))
 
-        #Getting the authors of the books
+        # Getting the authors of the books
         # if books:
         #     for b in books:
         #         authors = []
@@ -50,29 +50,23 @@ def search(request, keyword:str=None):
         #                 authors.append(a1.name)
         #             books_and_authors.append((b[0], b[1], authors))
 
-        context = {
-            'books': books_by_title,
-            'keyword': keyword
-            }
+        context = {'books': books_by_title, 'keyword': keyword}
 
         return render(request, 'books/search_result.html', context)
 
+
 def get_info(request, book_id):
+    promo_books = Book.objects.get(id=book_id)
 
-    authors_of_book = []
-    genres_of_book = []
+    authors = Author.objects.filter(book__title=promo_books.title)
+    authors = list(authors)
 
-    book_to_display = Book.objects.get(id=book_id)
-
-    authors_of_book_queryset = Author.objects.filter(book__title=book_to_display.title)
-    authors_of_book = list(authors_of_book_queryset)
-
-    genre_of_book_queryset = Genre.objects.filter(book__title=book_to_display.title)
-    genres_of_book = list(genre_of_book_queryset)
+    genres = Genre.objects.filter(book__title=promo_books.title)
+    genres = list(genres)
 
     context = {
-        'book_to_display': book_to_display,
-        'authors_of_book': authors_of_book,
-        'genres_of_book': genres_of_book,
+        'book': promo_books,
+        'authors_of_book': authors,
+        'genres_of_book': genres,
     }
     return render(request, 'books/book_info.html', context)
--- virtual_library/main_site/urls.py
+++ virtual_library/main_site/urls.py
@@ -7,13 +7,15 @@ from django.contrib.auth import views as auth_views
 
 from . import views
 
+login_template = {'template_name': 'main_site/signin_form.html'}
+
 app_name = 'main_site'
 urlpatterns = [
-    path('signin/', auth_views.login, {'template_name': 'main_site/signin_form.html'}, name='signin'),
+    path('signin/', auth_views.login, login_template, name='signin'),
     path('signup/', views.signup, name='signup'),
     path('logout/', auth_views.logout, {'next_page': '/'}, name='logout'),
     path('', views.index, name='index'),
     path('userprofile/', views.userprofile, name='userprofile'),
-    path('index/',  RedirectView.as_view(url='')),
+    path('index/', RedirectView.as_view(url='')),
     # re_path(r'', views.error, name='error'),
 ]
